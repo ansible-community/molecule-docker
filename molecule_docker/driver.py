007b8e36d282c6574b2ba5beb3fc79b35e57ba5a
Sorin Sbarnea <ssbarnea@redhat.com>
2021-05-26 09:28:36 +0100

    Bulk fixes
    Building all these containers does not really depend on python
    version of ansible version. This moves this test to a standalone pipeline.
    
    Starting with this version we only support and require latest community.docker collection and we assume user installed it correctly.
    
    Installing just ansible distribution is not enough to make this
    plugin functional.

--- molecule_docker/driver.py
+++ molecule_docker/driver.py
@@ -217,7 +217,7 @@ class Docker(Driver):
         return {"instance": instance_name}
 
     def ansible_connection_options(self, instance_name):
-        x = {"ansible_connection": "docker"}
+        x = {"ansible_connection": "community.docker.docker"}
         if "DOCKER_HOST" in os.environ:
             x["ansible_docker_extra_args"] = "-H={}".format(os.environ["DOCKER_HOST"])
         return x
